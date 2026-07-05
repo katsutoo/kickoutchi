10602f38902fb9c87c691544f66e32e2d0a45dbd
nugget <vincent.tantei@proton.me>
2026-07-05 14:16:55 +0200

    release v1.1.1

--- flake.nix
+++ flake.nix
@@ -23,7 +23,7 @@
         let
           kickoutchi = pkgs.rustPlatform.buildRustPackage {
             pname = "kickoutchi";
-            version = "1.1.0";
+            version = "1.1.1";
 
             src = ./.;
             cargoLock.lockFile = ./Cargo.lock;
